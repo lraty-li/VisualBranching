ca2d15a58c947888d894c01419496de0a6278ac0
Lraty <46740834+lraty-li@users.noreply.github.com>
2022-05-24 12:56:00 +0800

    ctlbtn save to head

---
+++ lib/providers/MainStatus.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:graphview/GraphView.dart';
+import 'package:visual_branching/util/models.dart';
+
+class MainStatus extends ChangeNotifier {
+  //opened repo, but only one repo supported now , maybe muti repo will be support
+  List<Repo> openedRepoList = [];
+  List<Graph> graphs=[];
+
+  updateVoidCall() {
+    notifyListeners();
+  }
+
+  /// todo make add list ,run notify listeners only once
+  add(Repo item) {
+    openedRepoList.add(item);
+    graphs.add(Graph()..isTree = true);
+    notifyListeners();
+  }
+
+  removeByKey(ValueKey targetRepoKey) {
+    openedRepoList.remove(openedRepoList
+        .firstWhere((element) => element.repoKey == targetRepoKey));
+        //todo 删除对应graph
+    notifyListeners();
+  }
+
+  removeAll() {
+    openedRepoList.clear();
+    graphs.clear();
+    notifyListeners();
+  }
+}
