c77a5f0dd03059a5ecca38149df440072789d8f6
Lraty <46740834+lraty-li@users.noreply.github.com>
2022-05-30 13:53:53 +0800

    update window bar btn color

--- lib/MainUi/window_title_bar.dart
+++ lib/MainUi/window_title_bar.dart
@@ -4,7 +4,7 @@ import 'package:visual_branching/Repository/repository_menu.dart';
 
 class WindowTitleBar extends StatelessWidget {
   final BuildContext pContext;
-  const WindowTitleBar({Key? key,required this.pContext}) : super(key: key);
+  const WindowTitleBar({Key? key, required this.pContext}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +22,16 @@ class WindowTitleBar extends StatelessWidget {
 }
 
 final buttonColors = WindowButtonColors(
-    iconNormal: const Color(0xFF805306),
-    mouseOver: const Color(0xFFF6A00C),
-    mouseDown: const Color(0xFF805306),
-    iconMouseOver: const Color(0xFF805306),
-    iconMouseDown: const Color(0xFFFFD500));
+    iconNormal: const Color(0xFF000000),
+    mouseOver: const Color(0xFF2196F3),
+    mouseDown: const Color(0xFF88C7F9),
+    iconMouseOver: const Color(0xFFFFFFFF),
+    iconMouseDown: const Color(0xFF007ACC));
 
 final closeButtonColors = WindowButtonColors(
+    iconNormal: const Color(0xFF000000),
     mouseOver: const Color(0xFFD32F2F),
     mouseDown: const Color(0xFFB71C1C),
-    iconNormal: const Color(0xFF805306),
     iconMouseOver: Colors.white);
 
 class WindowButtons extends StatelessWidget {
